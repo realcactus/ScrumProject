0c570d94e496cf479d764270e92345480a6f26d1
Songsong <15888382812@163.com>
2019-02-03 20:59:09 +0800

    Create Academy.java

---
+++ ScrumWeb/src/java/com/Discuz/Bean/Academy.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.Discuz.Bean;
+
+/**
+ *
+ * @author OovEver
+ */
+public class Academy {
+    private int id;
+    private String School;
+    private String academy;
+    private String Img;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the School
+     */
+    public String getSchool() {
+        return School;
+    }
+
+    /**
+     * @param School the School to set
+     */
+    public void setSchool(String School) {
+        this.School = School;
+    }
+
+    /**
+     * @return the academy
+     */
+    public String getAcademy() {
+        return academy;
+    }
+
+    /**
+     * @param academy the academy to set
+     */
+    public void setAcademy(String academy) {
+        this.academy = academy;
+    }
+
+    /**
+     * @return the Img
+     */
+    public String getImg() {
+        return Img;
+    }
+
+    /**
+     * @param Img the Img to set
+     */
+    public void setImg(String Img) {
+        this.Img = Img;
+    }
+    
+}
